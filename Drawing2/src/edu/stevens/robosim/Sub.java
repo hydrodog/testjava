c9a60f41c3930e2d3ec11097cf4e6b689c968b61
jxing2 <jxing2@users.noreply.github.com>
2015-11-13 17:00:20 -0500

    Update Sub.java

--- Drawing2/src/edu/stevens/robosim/Sub.java
+++ Drawing2/src/edu/stevens/robosim/Sub.java
@@ -4,13 +4,13 @@ public class Sub extends Robot{
 	double buoyancy;
 	
 	public Sub(Vector realposition, Vector realvelocity, Vector imageposition, Vector imagevelocity, double mass,
-			double battery_life, double max_cur, double max_vol, double theta, double buoyancy)
+			double battery_life, double max_cur, double max_vel, double theta, double buoyancy)
 	{
 		super(realposition, realvelocity,imageposition,imagevelocity, mass,battery_life,max_cur,max_vol,theta);
 		//pitch, voltage, buoyancy
 	}
 	
-	public void move(Vector v)
+	public void move(Vector vel, Vector water_vel)
 	{
 		
 	}
