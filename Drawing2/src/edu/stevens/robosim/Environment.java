5817fc01e2d481830a290b17fe04473cd5ca3d8e
zyan5 <zyan5@stevens.edu>
2015-11-13 16:27:49 -0500

    Define Environment

--- Drawing2/src/edu/stevens/robosim/Environment.java
+++ Drawing2/src/edu/stevens/robosim/Environment.java
@@ -1,5 +1,17 @@
 package edu.stevens.robosim;
 
 public abstract class Environment {
+	private double x;
+	private double y;
+	private double z;
+	private double r;
+	private double getX(){ return 0;}
+	private double getY(){ return 0;}
+	private double getZ(){ return 0;}
+	private static Random r(){};
+	public static int dieRoll() { return r.nextInt(6) + 1; }
 
+	
+	
+	
 }
