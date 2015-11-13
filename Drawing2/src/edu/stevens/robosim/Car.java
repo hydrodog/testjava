9f47d8b37b9498e9bf98ff3fbc123f9e8b3e8428
HengyuLiang <lianghengyu3@gmail.com>
2015-11-13 16:48:41 -0500

    modified car constructor

--- Drawing2/src/edu/stevens/robosim/Car.java
+++ Drawing2/src/edu/stevens/robosim/Car.java
@@ -3,10 +3,10 @@ package edu.stevens.robosim;
 public abstract class Car extends Robot{
 	private int NumWheel;
 	private double LRotVol,RRotVol;
-	public Car(Vector position, Vector velocity, double mass,
-			double batter_life, double max_cur, double max_vol, double theta,
+	public Car(Vector realposition, Vector realvelocity,Vector imageposition, Vector imagevelocity,
+			double mass,double batter_life, double max_cur, double max_vol, double theta,
 			int NumWheel,double LRotVol,double RRotVol){
-		super(position,velocity,mass,batter_life,max_cur,max_vol,theta);
+		super(realposition,realvelocity,imageposition,imagevelocity,mass,batter_life,max_cur,max_vol,theta);
 		this.NumWheel=NumWheel;
 		this.LRotVol=LRotVol;
 		this.RRotVol=RRotVol;
