7b10ac47f0871ae6292c2fd2d68dc0047e4cbdf4
EEElvaaaa <elva.lvyiwei@gmail.com>
2015-11-13 17:02:38 -0500

    update the max_vel

--- Drawing2/src/edu/stevens/robosim/Environment.java
+++ Drawing2/src/edu/stevens/robosim/Environment.java
@@ -1,27 +1,14 @@
 package edu.stevens.robosim;
 
-public abstract class Environment {
-	private double x;
-	private double y;
-	private double z;
-	private double r;
-	private double landheight;
-	private double waterdepth;
-	private double waterspeed;
-	private double airspeed;
-	private double airdirection;	
-	public double getX(){ return 0;}
-	public double getY(){ return 0;}
-	public double getZ(){ return 0;}
-	public static Random r(){};
-	public double getLandheight(){ return 0;}
-	public double getWaterdepth(){ return 0;}
-	public double getWaterspeed(){ return 0;}
-	public double getAirspeed(){ return 0;}
-	public double getAirdirection(){ return 0;}
-	public static int dieRoll() { return r.nextInt(6) + 1; }
-
-	
-	
-	
+public abstract class Environment 
+{
+	private maximum current;
+	private voltage;
+	private maximum velocity;
+	public Environment(int i; int v; int velocity)
+	{
+		maximun current = i;
+		voltage = v;
+		velocity = 
+	}
 }
