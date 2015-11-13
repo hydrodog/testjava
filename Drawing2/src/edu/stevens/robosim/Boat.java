70a1ed333fa50147d49c85d67c928d6702420fd3
sushmitade <sde@stevens.edu>
2015-11-13 16:39:58 -0500

    Created class Boat

--- Drawing2/src/edu/stevens/robosim/Boat.java
+++ Drawing2/src/edu/stevens/robosim/Boat.java
@@ -1,14 +1,30 @@
 package edu.stevens.robosim;
 
 public abstract class Boat extends Robot{
-	private double depth;	
-	
+	public Boat(Vector position, Vector velocity, double mass, double batter_life, double max_cur, double max_vol,
+			double theta) {
+		super(position, velocity, mass, batter_life, max_cur, max_vol, theta);
+	}
+	private double depth;
+		
 }
 
-public abstract class Speed extends Boat{
+public class Speed extends Boat{
+	public Speed(Vector position, Vector velocity, double mass, double batter_life, double max_cur, double max_vol,
+			double theta) {
+		super(velocity, velocity, theta, theta, theta, theta, theta);
+			}
 	private double mph;
 	private double accelerate;
 	private double decelerate;
+
+	public void pitch(double angle, double degree) {
+		
+	}
+
+	public void stop() {
+		
+	}
 }
 
 
