996a2a6662fcb18638570cef2763ee50fd265edf
sushmitade <sde@stevens.edu>
2015-11-13 16:20:58 -0500

    Created class Boat extending to class Robot.

---
+++ Drawing2/src/edu/stevens/robosim/Boat.java
@@ -0,0 +1,16 @@
+package edu.stevens.robosim;
+
+public abstract class Boat extends Robot{
+	private double depth;
+	
+	
+}
+
+public abstract class Speed extends Boat{
+	private double mph;
+	private double accelerate;
+	private double decelerate;
+}
+
+
+
