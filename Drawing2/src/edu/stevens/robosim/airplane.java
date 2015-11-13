d18d50bb446327843efecf41899d86a911ae5d6a
Jun ShangGuan <Jun@155.246.213.112>
2015-11-13 16:29:57 -0500

    Basic airplane

---
+++ Drawing2/src/edu/stevens/robosim/airplane.java
@@ -0,0 +1,24 @@
+package edu.stevens.robosim;
+
+public class airplane{
+	
+	private static float x,y,z,vx,vy,vz,time;
+	
+	airplane(float x,float y,float z,float time){
+		this.x=x;this.y=y;this.z=z;this.time=time;
+	}
+	public void move(float i,float j,float m,float time){
+		try{
+		x-=i;y-=j;z-=m;this.time-=time;
+		Thread.sleep(500);
+		}catch(Exception e)
+		{e.printStackTrace();}
+	}
+	public static void main(String[] args) throws Exception{
+		airplane robot=new airplane(100,100,100,20);
+		while(x>0 && y>0 && z>0 && time>=0){
+			robot.move(10, 10, 10, 3);
+			System.out.println("The airplane position is: "+x+","+y+","+z);
+		}
+	}
+}
