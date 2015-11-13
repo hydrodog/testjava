c9064512684df9298671a79fc2d39795da1f9a5c
sushmitade <sde@stevens.edu>
2015-11-13 17:11:27 -0500

    Updated changes in class Boat.

--- Drawing2/src/edu/stevens/robosim/Boat.java
+++ Drawing2/src/edu/stevens/robosim/Boat.java
@@ -1,30 +1,16 @@
 package edu.stevens.robosim;
 
-public abstract class Boat extends Robot{
-	public Boat(Vector position, Vector velocity, double mass, double batter_life, double max_cur, double max_vol,
-			double theta) {
-		super(position, velocity, mass, batter_life, max_cur, max_vol, theta);
+public abstract class Boat extends Environment{
+	class Depth extends Environment{
+	public Depth(double waterdepth, double waterspeed, double landheight){
+	super();
 	}
-	private double depth;
-		
 }
-
-public class Speed extends Boat{
-	public Speed(Vector position, Vector velocity, double mass, double batter_life, double max_cur, double max_vol,
-			double theta) {
-		super(velocity, velocity, theta, theta, theta, theta, theta);
-			}
-	private double mph;
-	private double accelerate;
-	private double decelerate;
-
-	public void pitch(double angle, double degree) {
-		
-	}
-
-	public void stop() {
-		
-	}
+	
+	
+	
+	
+	
 }
 
 
