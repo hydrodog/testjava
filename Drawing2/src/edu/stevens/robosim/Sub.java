bf674558ec0689155e1937c874611aaff7bf4230
OnigiriFan <fanxiaoyu3212gmail.com>
2015-11-13 16:29:19 -0500

    test

--- Drawing2/src/edu/stevens/robosim/Sub.java
+++ Drawing2/src/edu/stevens/robosim/Sub.java
@@ -2,8 +2,17 @@ package edu.stevens.robosim;
 
 public class Sub extends Robot{
 	double buoyancy;
+	double weight;
+	
 	public Robot()
 	{
+		
+	}
+	public void ChangeWeight(double w){
+		weight += w;
+	}
+	
+	public double RisingSpeed( weight ){//Z speed
 		
 	}
 }
