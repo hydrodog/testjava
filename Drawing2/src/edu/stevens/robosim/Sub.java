d4b1d04408b98dec8fe40ff1512eb4130f2743a6
jxing2 <jxing2@users.noreply.github.com>
2015-11-13 16:41:19 -0500

    Update Sub.java

--- Drawing2/src/edu/stevens/robosim/Sub.java
+++ Drawing2/src/edu/stevens/robosim/Sub.java
@@ -3,8 +3,8 @@ package edu.stevens.robosim;
 public class Sub extends Robot{
 	double buoyancy;
 	
-	public Sub(Vector pos, Vector vel, int theta, int pitch, 
-			int mass, int battery, int battery_max, int voltage, int vel_max, double buoyancy)
+	public Sub(Vector pos, Vector vel, double theta, double pitch, 
+			double mass, double battery, double battery_max, double voltage, double vel_max, double buoyancy)
 	{
 		super(pos, vel,mass,battery, battery_max,vel_max,theta);
 		//pitch, voltage, buoyancy
