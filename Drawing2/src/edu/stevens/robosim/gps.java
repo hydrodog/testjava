cc3e3e9f12849dfc1ff8f6afbc414379feee4725
pvishwak <pvishwak@stevens.edu>
2015-11-16 21:30:28 -0500

    Position and gaussian error calculated

--- Drawing2/src/edu/stevens/robosim/gps.java
+++ Drawing2/src/edu/stevens/robosim/gps.java
@@ -2,6 +2,28 @@ package edu.stevens.robosim;
 import java.util.Vector;
 
 
-public class gps {
+public abstract class gps {
 
-}
+
+
+	private PositionRadians lat, lon, true position, gaussian error, estimate position, sigma, y, a;
+	y = math.sqrt(2*3.14);
+	a= 1/y*sigma;
+	gaussian error=
+	
+	public Position() {
+		this(0,0);
+	}
+	public Position(double la, double lo) {
+		lat = new PositionRadians(la);
+		lon = new PositionRadians(lo);
+	}
+	
+	
+	public Position(IPosition pos) {
+		lat = pos.getLatitude();
+		lon = pos.getLongitude();
+	}
+	
+	
+	
