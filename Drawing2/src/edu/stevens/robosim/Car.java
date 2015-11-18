e3dcf77d4e82c9995cf3eb3f202a415bc1afc0f3
HengyuLiang <lianghengyu3@gmail.com>
2015-11-17 22:12:54 -0500

    Change some errors

--- Drawing2/src/edu/stevens/robosim/Car.java
+++ Drawing2/src/edu/stevens/robosim/Car.java
@@ -1,15 +1,12 @@
+
+
 package edu.stevens.robosim;
 
 public abstract class Car extends Robot{
-	private int NumWheel;
-	private double LRotVol,RRotVol;
-	public Car(Vector realposition, Vector realvelocity,Vector imageposition, Vector imagevelocity,
-			double mass,double batter_life, double max_cur, double max_vol, double theta,
-			int NumWheel,double LRotVol,double RRotVol){
-		super(realposition,realvelocity,imageposition,imagevelocity,mass,batter_life,max_cur,max_vol,theta);
-		this.NumWheel=NumWheel;
-		this.LRotVol=LRotVol;
-		this.RRotVol=RRotVol;
+	public Car(Vector realposition, Vector realvelocity, Vector imageposition, Vector imagevelocity, double mass,
+			double batter_life, double max_cur, double max_vel, double theta, double voltage,
+			int NumWheel){
+		super(realposition,realvelocity,imageposition,imagevelocity,mass,batter_life,max_cur,max_vel,theta,voltage);
 	}
 	
 }
