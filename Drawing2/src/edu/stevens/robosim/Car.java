f98a604b3e6d7bcb6f497ac00e0826697381f060
ShuaiLu <slu6@stevens.edu>
2015-11-19 17:19:44 -0500

    Modify the move method

--- Drawing2/src/edu/stevens/robosim/Car.java
+++ Drawing2/src/edu/stevens/robosim/Car.java
@@ -12,17 +12,24 @@ public class Car extends Robot{
 			double batter_life, double max_cur, double max_vel, double theta, double voltage){
 		super(realposition,realvelocity,imageposition,imagevelocity,mass,batter_life,max_cur,max_vel,theta,voltage);
 	}
+	//It there is water return true.
 	public boolean Water(){
-		if(waterdepth!=0){
+		if(getRealPosition().getZ()<0){
 			return true;
 		}
 		return false;
 	}
 	// If a want to turn, just set the Carspeed to a unit Vector. And the speed of the car would be the length of Vector Carspeed.
+	// And if there is water it will stop();
 	public void move(Vector CarSpeed){
-		Vector v = getRealPosition();
-		setImageVelocity(CarSpeed);
-		setRealPosition(v.add(CarSpeed));
+		if (Water()==true){
+			stop();
+		}else{
+			Vector v = getRealPosition();
+			setImageVelocity(CarSpeed);
+			setRealPosition(v.add(CarSpeed));
+		}
+		
 	}
 	//To stop the car, we can set the real_velocity to (0,0,0).
 	public void stop(){
