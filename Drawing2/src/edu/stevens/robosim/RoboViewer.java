59b669ece073c7b3195f6a201cc4437020aca3dd
srushti69 <srushti_69@yahoo.com>
2015-11-20 14:45:28 -0500

    It will indicate possition of robo

--- Drawing2/src/edu/stevens/robosim/RoboViewer.java
+++ Drawing2/src/edu/stevens/robosim/RoboViewer.java
@@ -1,5 +1,18 @@
 package edu.stevens.robosim;
 
-public class RoboViewer {
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
-}
+import javax.swing.JPanel;
+
+public class RoboViewer  {
+	
+		private int startX,startY,endX,endY;
+		private ArrayList arraylst;
+		public void draw(Graphics g) {
+			g.drawOval(x,y,10,10);
+		}
+}|
+
